3add5a8a186de379897c574bf5c016eff9651a79
2022Jean <6159984@gmail.com>
2023-10-11 14:25:14 +0800

    为了方便测试，修改根链接下的代码

--- myblog/view/user.py
+++ myblog/view/user.py
@@ -1,8 +1,12 @@
+import redis
 from flask import Blueprint, current_app, redirect, render_template, request, url_for
 
+from myblog.model import pool
 from myblog.model.item import Post
 
 user = Blueprint("user", __name__)
+
+conn = redis.Redis(connection_pool=pool)
 
 
 @user.route("/read")
@@ -15,4 +19,6 @@ def read_post():
 
 @user.route("/")
 def home():
-    return redirect(url_for(".read_post", id="MUDf060"))
+    recent_post_id: bytes = conn.zrange("post:recent", 0, -1)[0]
+
+    return redirect(url_for(".read", id=recent_post_id.decode()))
