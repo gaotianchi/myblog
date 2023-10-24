42f2c25153c7d1e01ccdd29e4f010298178c54b0
高天驰 <6159984@gmail.com>
2023-10-24 15:58:30 +0800

    添加功能函数 读取 json 文件

--- myblog/model/utlis.py
+++ myblog/model/utlis.py
@@ -4,6 +4,7 @@
 
 import base64
 import hashlib
+import json
 import re
 
 import yaml
@@ -51,3 +52,12 @@ def get_summary_and_body(git_message: str) -> dict:
     summary, body = items
 
     return dict(summary=summary.strip(), body=body.strip())
+
+
+def get_data_from_json_file(path: str) -> dict:
+    """
+    职责：从 json 文件中读取数据
+    """
+    with open(path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
