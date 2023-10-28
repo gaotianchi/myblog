7e7c02756a7cb22e9f049a3e9bf39ab7f4f08dff
高天驰 <6159984@gmail.com>
2023-10-28 11:45:21 +0800

    更新 mysql 操作句柄

--- myblog/model/db/handler.py
+++ myblog/model/db/handler.py
@@ -12,9 +12,18 @@ class MySQLHandler:
 
     def __init__(self, host, user, password, database) -> None:
         self.connection = pymysql.connect(
-            host=host, user=user, password=password, database=database
+            host=host, user=user, password=password, database=None
         )
         self.cursor = self.connection.cursor()
+        self.database = database
+
+        if database:
+            self.create_database_if_not_exists(database)
+            self.connection.select_db(database)
+
+    def create_database_if_not_exists(self, database):
+        create_database_query = f"CREATE DATABASE IF NOT EXISTS {database}"
+        self.cursor.execute(create_database_query)
 
     def get(self, query) -> tuple[tuple]:
         self.cursor.execute(query)
