f987168665494622d457b6d5bf3a16854287685a
高天驰 <6159984@gmail.com>
2023-10-28 09:37:13 +0800

    添加 mysql 操作句柄

--- myblog/model/db/handler.py
+++ myblog/model/db/handler.py
@@ -2,8 +2,29 @@
 职责：定义数据库操作句柄
 """
 
+import pymysql
 
-class MysqlHandler:
+
+class MySQLHandler:
     """
     职责：操作 mysql 数据库
     """
+
+    def __init__(self, host, user, password, database) -> None:
+        self.connection = pymysql.connect(
+            host=host, user=user, password=password, database=database
+        )
+        self.cursor = self.connection.cursor()
+
+    def get(self, query) -> tuple[tuple]:
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        return result
+
+    def modify(self, query) -> None:
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def close(self) -> None:
+        self.cursor.close()
+        self.connection.close()
