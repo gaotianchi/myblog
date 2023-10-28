f2ae2af649693a7ea87e0e88f93036520bec119e
高天驰 <6159984@gmail.com>
2023-10-28 09:05:02 +0800

    尝试构建 restful 风格的 api

--- myblog/view/api.py
+++ myblog/view/api.py
@@ -1,3 +1,35 @@
 from flask import Blueprint
+from flask.views import MethodView
 
-api = Blueprint("api", __name__)
+api = Blueprint("api", __name__, subdomain="api")
+
+
+class PostAPI(MethodView):
+    init_every_request = False
+
+    def __init__(self, model):
+        self.model = model
+
+    def get(self):
+        """
+        职责：处理 GET 请求，返回单个文章数据
+        """
+        ...
+
+    def post(self):
+        """
+        职责：处理 POST 请求，创建新文章
+        """
+        ...
+
+    def patch(self):
+        """
+        职责：处理 PATCH 请求，更新已存在的文章
+        """
+        ...
+
+    def delete(self):
+        """
+        职责：处理 DELETE 请求，删除指定文章
+        """
+        ...
