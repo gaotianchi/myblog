d39ed23606c4a9dbcddc16d2c37b85fe3851efab
高天驰 <6159984@gmail.com>
2023-10-31 18:46:53 +0800

    添加函数解密数据

--- myblog/utlis.py
+++ myblog/utlis.py
@@ -2,6 +2,8 @@ import base64
 import hashlib
 from datetime import date, datetime
 
+from cryptography.fernet import Fernet
+
 
 def json_serial(obj):
     if isinstance(obj, (datetime, date)):
@@ -15,3 +17,13 @@ def generate_id(title: str) -> str:
     title_id = base64.urlsafe_b64encode(hash_digest)[:20].decode()
 
     return title_id
+
+
+def decrypt_data(key: bytes, encrypted_data: bytes) -> str:
+    """
+    职责：解密数据
+    """
+    fernet = Fernet(key)
+    decrypted_data = fernet.decrypt(encrypted_data)
+
+    return decrypted_data.decode("utf-8")
