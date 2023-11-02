6029fcb8c9a756f956bb92fa5f3d7fac73aef79a
高天驰 <6159984@gmail.com>
2023-11-02 21:54:08 +0800

    Add flask extensions medule

---
+++ myblog/flaskexten.py
@@ -0,0 +1,14 @@
+"""
+Abstract: This module defines the FLASK extension.
+
+Author: Gao Tianchi
+Contact: 6159984@gmail.com
+Version: 0.3
+Creation date: 2023-11-02
+Latest modified date: 2023-11-02
+Copyright (C) 2023 Gao Tianchi
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
