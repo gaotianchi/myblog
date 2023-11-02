c5fa4b03409c5082346914db46a184643b5c3b98
高天驰 <6159984@gmail.com>
2023-11-02 21:52:29 +0800

    Add database table POST

---
+++ myblog/model/database/table.py
@@ -0,0 +1,31 @@
+"""
+Abstract: This module defines the database table structure.
+
+Required: myblog.flaskexten.db
+
+Author: Gao Tianchi
+Contact: 6159984@gmail.com
+Version: 0.3
+Creation date: 2023-11-02
+Latest modified date: 2023-11-02
+Copyright (C) 2023 Gao Tianchi
+"""
+
+from datetime import date
+
+from sqlalchemy import Date, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from myblog.flaskexten import db
+
+
+class PostTable(db.Model):
+    id: Mapped[str] = mapped_column(String(20), primary_key=True)
+    title: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    toc: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(50), nullable=False)
+    release: Mapped[date] = mapped_column(Date, default=date.today())
+    updated: Mapped[date] = mapped_column(Date, default=date.today())
+    summary: Mapped[str] = mapped_column(Text, nullable=False)
+    category: Mapped[str] = mapped_column(String(30), nullable=False)
