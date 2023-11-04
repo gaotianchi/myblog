e1399fbb30d4565e62788b529119740e4090cbb8
Gao Tianchi <6159984@gmail.com>
2023-11-04 17:16:56 +0800

    Add owner view function update_post

--- myblog/controller/owner.py
+++ myblog/controller/owner.py
@@ -40,3 +40,23 @@ def add_post():
         logger.error(f"Fail to create {now_post}!")
 
         return "Fail"
+
+
+@owner_bp.route("/update-post/<post_id>", methods=["PATCH"])
+@login_required
+def update_post(post_id: str):
+    post = Post.query.get_or_404(post_id)
+
+    post.update_from_dict(json.loads(request.json))
+
+    try:
+        db.session.add(post)
+        db.session.commit()
+        logger.info(f"Article {post} was updated successfully.")
+
+        return post.to_json()
+    except:
+        db.session.rollback()
+        logger.error(f"Fail to update {post}")
+
+        return "Fail"
