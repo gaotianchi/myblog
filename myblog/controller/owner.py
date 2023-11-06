60311b426144d9b0d29502a29a00e759dd218990
Gao Tianchi <6159984@gmail.com>
2023-11-06 15:03:03 +0800

    Delete old view function and add auth methods for owner

--- myblog/controller/owner.py
+++ myblog/controller/owner.py
@@ -11,11 +11,10 @@ Copyright (C) 2023 Gao Tianchi
 import json
 import logging
 
-from flask import Blueprint, request
-from flask_login import login_required
+from cryptography.fernet import Fernet
+from flask import Blueprint, abort, current_app, g, request
 
-from myblog.flaskexten import db
-from myblog.model.database.db import Post
+from myblog.model.database.db import Owner
 
 owner_bp = Blueprint("owner", __name__)
 
@@ -23,57 +22,26 @@ owner_bp = Blueprint("owner", __name__)
 logger = logging.getLogger("controller")
 
 
-@owner_bp.route("/add-post", methods=["POST"])
-@login_required
-def add_post():
-    now_post = Post.insert_from_dict(json.loads(request.json))
+def decrypt_token(secret_key: bytes, token: str) -> str:
+    fernet = Fernet(secret_key)
+    decrypted_data = fernet.decrypt(token.encode("utf-8"))
 
-    try:
-        db.session.add(now_post)
-        db.session.commit()
-        logger.info(f"Article {now_post} was created successfully.")
-
-        return now_post.to_json()
-    except:
-        db.session.rollback()
-        logger.error(f"Fail to create {now_post}!")
-
-        return f"Fail to create {now_post}!"
+    return decrypted_data.decode("utf-8")
 
 
-@owner_bp.route("/update-post/<post_id>", methods=["PATCH"])
-@login_required
-def update_post(post_id: str):
-    post = Post.query.get_or_404(post_id)
+@owner_bp.before_request
+def load_user():
+    token: str = request.args.get("token")
+    if token:
+        try:
+            decrypted_data: str = decrypt_token(current_app.config["SECRET_KEY"], token)
+            data: dict = json.loads(decrypted_data)
+            owner = Owner.query.get(data["name"])
+            g.owner = owner
 
-    post.update_from_dict(json.loads(request.json))
+            logger.info(f"Login successfully.")
+        except:
+            logger.info(f"Fail to Login!")
+            abort(401)
 
-    try:
-        db.session.add(post)
-        db.session.commit()
-        logger.info(f"Article {post} was updated successfully.")
-
-        return post.to_json()
-    except:
-        db.session.rollback()
-        logger.error(f"Fail to update {post}")
-
-        return f"Fail to update {post}"
-
-
-@owner_bp.route("/delete-post/<post_id>", methods=["DELETE"])
-@login_required
-def delete_post(post_id: str):
-    post = Post.query.get_or_404(post_id)
-
-    try:
-        db.session.delete(post)
-        db.session.commit()
-        logger.info(f"Article {post} was deleted successfully.")
-
-        return f"Article {post} was deleted successfully."
-    except:
-        db.session.rollback()
-        logger.error(f"Fail to delete {post}")
-
-        return f"Fail to delete {post}"
+    return None
