ab4d15dd52495b12cffd96409ae3a5c4edd38844
Gao Tianchi <6159984@gmail.com>
2023-11-07 09:27:57 +0800

    Add class PostRender to render post body and toc

--- myblog/model/mdrender/itemrender.py
+++ myblog/model/mdrender/itemrender.py
@@ -8,9 +8,36 @@ Creation date: 2023-11-05
 Copyright (C) 2023 Gao Tianchi
 """
 
+from myblog.help import get_post_items
+
 from .render import Render
 
 
 class PostRender:
     def __init__(self) -> None:
         self.render = Render()
+        self.__data = {
+            "body": "",
+            "toc": "",
+        }
+
+    def set(self, path: str) -> None:
+        with open(path, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        items: dict = get_post_items(content)
+
+        self.body: dict = items["body"]
+
+    def __render_post(self) -> None:
+        body = self.render(self.body)
+        toc = self.render.toc
+
+        self.__data["body"] = body
+        self.__data["toc"] = toc
+
+    @property
+    def data(self) -> dict:
+        self.__render_post()
+
+        return self.__data
