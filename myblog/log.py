ca5944d1510d28f7776e1dfa90546300cc2a28c0
Gao Tianchi <6159984@gmail.com>
2023-11-20 12:12:10 +0800

    Add logger.

---
+++ myblog/log.py
@@ -0,0 +1,38 @@
+"""
+Summary: Configure the logging system.
+Created: 2023-11-19
+Author: Gao Tianchi
+"""
+
+import logging
+
+from .config import get_config
+
+config = get_config()
+
+# Set formatters
+default_formatter = logging.Formatter(
+    "%(asctime)s-[%(levelname)s]-[%(module)s]-[%(lineno)d]-%(message)s"
+)
+
+# Set handlers
+file_handler = logging.FileHandler(config.PATH_LOG.joinpath("app.log"))
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(default_formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(default_formatter)
+
+
+# Set loggers
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+root.addHandler(file_handler)
+root.addHandler(console_handler)
+
+
+def get_logger(name: str = None) -> logging.Logger:
+    match name:
+        case _:
+            return root
